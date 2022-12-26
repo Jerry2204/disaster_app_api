832721741b19433a8d11107057b9e59543bb4a6f
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-12-26 23:05:46 +0700

    logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -49,6 +49,10 @@ class AuthController extends Controller
 
     public function logout()
     {
-        return response()->json('This is my log out method');
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => 'You have successfully been logged out and your token has been deleted'
+        ]);
     }
 }
