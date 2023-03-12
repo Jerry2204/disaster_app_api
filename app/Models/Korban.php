5bf5c015e156c5565c1765dfc057a4aa5b93fbb4
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-03-12 12:27:54 +0700

    update model Korban

--- app/Models/Korban.php
+++ app/Models/Korban.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Korban extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'meninggal',
+        'luka_berat',
+        'luka_ringan',
+        'hilang',
+        'user_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
