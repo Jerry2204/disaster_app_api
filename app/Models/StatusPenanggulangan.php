db59fb28f864d306f499222461d112f2aa2b5611
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-03-12 12:30:54 +0700

    update model status penanggulangan

--- app/Models/StatusPenanggulangan.php
+++ app/Models/StatusPenanggulangan.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class StatusPenanggulangan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'petugas',
+        'keterangan',
+        'tindakan',
+        'status',
+        'user_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
