43efe11802a344bf6b4881a6adc0024eaab3d26c
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-03-14 08:23:56 +0700

    create middleware for check role user

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Traits\HttpResponses;
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckRole
+{
+    use HttpResponses;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (in_array($request->user->role, $roles)) {
+            return $next($request);
+        }
+
+        return $this->error('', 'Kamu tidak memiliki otorisasi', 403);
+    }
+}
