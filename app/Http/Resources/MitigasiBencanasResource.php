c00f91df65d388e951e15b8662f067413087899d
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-03-14 20:18:44 +0700

    create resource file for mitigasi bencana

---
+++ app/Http/Resources/MitigasiBencanasResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MitigasiBencanasResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (string)$this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'deskripsi' => $this->deskripsi,
+                'jenis_konten' => $this->jenis_konten,
+                'file' => $this->file,
+            ],
+            'relationships' => [
+                'user_id' => $this->user->id,
+                'user_name' => $this->user->name,
+                'user_email' => $this->user->email
+            ]
+        ];
+    }
+}
