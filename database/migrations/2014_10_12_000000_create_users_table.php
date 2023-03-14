b84670e08d75698bf5e4a7c980e2c367ddfb401f
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-03-14 09:02:21 +0700

    add role field in table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'pra_bencana', 'tanggap_darurat', 'pasca_bencana', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
