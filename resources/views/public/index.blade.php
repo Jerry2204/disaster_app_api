558000ba923992e69fdad49b591a21ae00ac6262
evanhutagaol <evanhutagaol15@gmail.com>
2023-05-01 19:54:45 +0700

    initial commit

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -7,8 +7,8 @@
             <div class="row">
                 <div class="col-md-6 col-sm-5 text-start d-flex flex-column justify-content-center left-hero">
                     <h1 class="text-hero">Anda Dalam Keadaan Darurat?</h1>
-                    <p class="subtext-hero">Sampaikan Laporan Peristiwa Darurat di Sekitar Anda!</p><a
-                        href="{{ route('report.add') }}" style="text-decoration: none; color: white;">
+                    <p class="subtext-hero">Sampaikan Laporan Peristiwa Darurat di Sekitar Anda!</p>
+                    <a href="{{ route('report.add') }}" style="text-decoration: none; color: white;">
                         <button class="btn-custom-danger"><strong>LAPOR!</strong></button></a>
                 </div>
                 <div class="col-md-6 col-sm-7 right-hero">
@@ -22,11 +22,12 @@
                                 </path>
                             </svg>
                             <hr class="hr-peringatan">
-                                <div class="p-3 fs-6"><b
-                                        style="font-family: Inter;">{{ \Carbon\Carbon::parse($newestPeringatan->tanggal)->locale('id-ID')->format('d F Y') }} |
-                                        {{ $newestPeringatan->lokasi }} </b>
-                                    <p class="mt-3" style="font-family: Inter;">{{ $newestPeringatan->deskripsi }}</p>
-                                </div>
+                            <div class="p-3 fs-6"><b
+                                    style="font-family: Inter;">{{ \Carbon\Carbon::parse($newestPeringatan->tanggal)->locale('id-ID')->format('d F Y') }}
+                                    |
+                                    {{ $newestPeringatan->lokasi }} </b>
+                                <p class="mt-3" style="font-family: Inter;">{{ $newestPeringatan->deskripsi }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -137,43 +138,43 @@
 
     <section class="bg-blue text-black p-1">
         @if (count($newestArtikel) > 0)
-        <div class="container">
-            <div class="col-md-12 mt-5 mb-5">
-                <div class="my-5">
-                    <h3 class="mb-5 text-white text-center">Pengumuman</h3>
-                    <div class="row">
-                        @foreach ($newestArtikel as $item)
-                            <div class="col-md-3 mb-3">
-                                <div class="card text-start" style="padding: 0px;">
-                                    <img src="{{ asset('artikel/' . $item->gambar) }}" alt="{{ $item->gambar }}"
-                                        style="border-radius: 5px 5px 0px 0px;">
-                                    <div class="card-body">
-                                        <h6 class="card-title text-start">
-                                            {{ $item->judul }}
-                                        </h6>
-                                        <p class="card-subtitle mb-2 text-muted ">
-                                            {{ \Carbon\Carbon::parse($item->created_at)->locale('id-ID')->format('d F Y') }}
-                                        </p>
-                                        <button class="btn btn-primary mt-5" style="background-color: rgb(2, 85, 165);">
+            <div class="container">
+                <div class="col-md-12 mt-5 mb-5">
+                    <div class="my-5">
+                        <h3 class="mb-5 text-white text-center">Pengumuman</h3>
+                        <div class="row">
+                            @foreach ($newestArtikel as $item)
+                                <div class="col-md-3 mb-3">
+                                    <div class="card text-start" style="padding: 0px;">
+                                        <img src="{{ asset('artikel/' . $item->gambar) }}" alt="{{ $item->gambar }}"
+                                            style="border-radius: 5px 5px 0px 0px;">
+                                        <div class="card-body">
+                                            <h6 class="card-title text-start">
+                                                {{ $item->judul }}
+                                            </h6>
+                                            <p class="card-subtitle mb-2 text-muted ">
+                                                {{ \Carbon\Carbon::parse($item->created_at)->locale('id-ID')->format('d F Y') }}
+                                            </p>
+                                            <button class="btn btn-primary mt-5" style="background-color: rgb(2, 85, 165);">
 
-                                            <a href="{{ route('artikel.detail', $item->id) }}"
-                                                style="text-decoration: none; color: white;">
-                                                Baca Selengkapnya
-                                            </a>
-                                        </button>
+                                                <a href="{{ route('artikel.detail', $item->id) }}"
+                                                    style="text-decoration: none; color: white;">
+                                                    Baca Selengkapnya
+                                                </a>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
-                    </div>
+                            @endforeach
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
         @else
-        <br>
-        <h3 class="mb-5 text-white text-center">Tidak Ada Pengumuman Terkini</h3>
-    @endif
+            <br>
+            <h3 class="mb-5 text-white text-center">Tidak Ada Pengumuman Terkini</h3>
+        @endif
     </section>
     {{-- <div class="container">
         <div class="row no- gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
