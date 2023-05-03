7a866f6c192f4f4facd9ac76badc4cf39cd422cd
SamuelSimanjuntak <82867282+SamuelSimanjuntak18@users.noreply.github.com>
2023-05-03 13:44:44 +0700

    news

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -145,12 +145,12 @@
                         </div>
                     </div>
                 </div>
-                <div class="fixed-button">
+                {{-- <div class="fixed-button">
                     <a href="https://codedthemes.com/item/guru-able-admin-template/" target="_blank"
                         class="btn btn-md btn-primary">
                         <i class="fa fa-shopping-cart" aria-hidden="true"></i> Upgrade To Pro
                     </a>
-                </div>
+                </div> --}}
             </div>
         </div>
 
