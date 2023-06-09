8f4356a834e2e38d6fcd9f6082afe9920cf72fb7
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-06-09 08:32:47 +0700

    update relation

--- app/Models/desa.php
+++ app/Models/desa.php
@@ -22,7 +22,7 @@ class desa extends Model
     }
 
     public function kecamatan() {
-        return $this->belongsTo(kecamatan::class);
+        return $this->belongsTo(Kecamatan::class);
     }
 
 
