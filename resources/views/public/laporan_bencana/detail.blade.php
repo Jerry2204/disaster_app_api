e31cd81e1b2f435dbac50e5e46deba19f51710bd
SamuelSimanjuntak <82867282+SamuelSimanjuntak18@users.noreply.github.com>
2023-06-15 13:46:43 +0700

    news

--- resources/views/public/laporan_bencana/detail.blade.php
+++ resources/views/public/laporan_bencana/detail.blade.php
@@ -134,7 +134,7 @@
                                     <b>Gambar kerusakan infrastruktur Saat Kejadian:</b>
                                     @if (!empty($laporanBencana->gambar_kejadian))
                                         @foreach (json_decode($laporanBencana->gambar_kejadian) as $image)
-                                            <img src="{{ asset('laporan/' . $image) }}" style="width: 100%; height: auto; display: block; margin-bottom: 10px;" alt="">
+                                            <img src="{{ asset('laporan/' . $image) }}" style="width: 50%; height: auto; display: block; margin-bottom: 10px;" alt="">
                                         @endforeach
                                     @else
                                         <p>Gambar Kerusakan Belum Ditambahkan</p>
@@ -144,7 +144,7 @@
                                     <b>Gambar kerusakan infrastruktur Sesudah Kejadian:</b>
                                     @if (!empty($laporanBencana->gambar_pasca))
                                         @foreach (json_decode($laporanBencana->gambar_pasca) as $image)
-                                            <img src="{{ asset('laporan/' . $image) }}" style="width: 100%; height: auto; display: block; margin-bottom: 10px;" alt="">
+                                            <img src="{{ asset('laporan/' . $image) }}" style="width: 50%; height: auto; display: block; margin-bottom: 10px;" alt="">
                                         @endforeach
                                     @else
                                         <p>Gambar Kerusakan Pasca Bencana Belum Ditambahkan</p>
